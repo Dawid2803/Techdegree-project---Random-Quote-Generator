e540d200783f03ea4ce0c38e4441249a805c9d07
Dawid2803 <dawidjacoba@gmail.com>
2021-08-05 20:48:21 +0200

    quotes array and getRandomQuote function completed

--- js/script.js
+++ js/script.js
@@ -10,14 +10,26 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+	{ quote: "I nearly had Everything!" , source: "Thomas Shelby", citation: "Peaky Blinders" },	
+	{ quote: "The journey of a thousand miles begins with one step.", source: "Lao Tzu", year: 2020 },
+	{ quote: "Never let the fear of striking out keep you from playing the game.", source: "Babe Ruth"},
+	{ quote: "Strive not to be a success, but rather to be of value.", source: "Albert Einstein"},
+	{ quote: "Those who dare to fail miserably can achieve greatly.", source: "John F. Kennedy"},
+]
 
 /***
  * `getRandomQuote` function
-***/
+***/ 
+function getRandomQuote(quoteArray){
+	let randomNumber = Math.floor(Math.random() * quoteArray.length);
+	console.log(randomNumber);
+	return quoteArray[randomNumber];
+	console.log(quoteArray[randomNumber]);
 
+}
 
+console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function

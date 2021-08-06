ff82642324a1cab5db68e20511fa08f79b9e76a9
Dawid2803 <dawidjacoba@gmail.com>
2021-08-06 18:30:05 +0200

    completed Random Quote generator

--- js/script.js
+++ js/script.js
@@ -9,41 +9,44 @@ project 1 - A Random Quote Generator
 
 /*** 
  * `quotes` array 
+	The quotes array consists of different objects along with their properties.
 ***/
 const quotes = [
 	{ quote: "I nearly had Everything!" , source: "Thomas Shelby", citation: "Peaky Blinders" },	
 	{ quote: "The journey of a thousand miles begins with one step.", source: "Lao Tzu", year: 2020 },
 	{ quote: "Never let the fear of striking out keep you from playing the game.", source: "Babe Ruth"},
-	{ quote: "Strive not to be a success, but rather to be of value.", source: "Albert Einstein"},
+	{ quote: "Strive not to be a success, but rather to be of value.", source: "Albert Einstein", tag: "Smartest man to have lived"},
 	{ quote: "Those who dare to fail miserably can achieve greatly.", source: "John F. Kennedy"},
 ]
 
 /***
  * `getRandomQuote` function
+	This function will use a random number to pick one of the quotes available in the quotes array.
 ***/ 
 function getRandomQuote(quoteArray){
 	let randomNumber = Math.floor(Math.random() * quoteArray.length);
-	console.log(randomNumber);
 	return quoteArray[randomNumber];
-	console.log(quoteArray[randomNumber]);
 
 }
 
-console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function
+	This function will print the randomly chosen quote to the screen.
 ***/
 function printQuote(){
 	let randomquote = getRandomQuote(quotes);
 	let quoteToHTML = `
-	<p>${randomquote.quote}</p>
-	<p>${randomquote.source}`;
+	<p class = "quote">${randomquote.quote}</p>
+	<p class = "source">${randomquote.source}`;
 	if (randomquote.citation){
-		quoteToHTML += `<span>, ${randomquote.citation}</span>`;
+		quoteToHTML += `<span class = "citation"> ${randomquote.citation}</span>`;
 		}
 	if(randomquote.year){
-		quoteToHTML += `<span>, ${randomquote.year}</span>`;
+		quoteToHTML += `<span class = "year"> ${randomquote.year}</span>`;
+	}
+	if(randomquote.tag){
+		quoteToHTML += `<span class = "tag">, ${randomquote.tag}</span>`;
 	}
 	quoteToHTML += `</p>`;
 	return document.getElementById('quote-box').innerHTML = quoteToHTML;
